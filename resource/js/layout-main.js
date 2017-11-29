c505fb747a9c01576ad95bc2d2cd2c0a20f4f10c
ShaoEnChen <seanchen47@gmail.com>
2017-11-30 02:08:42 +0800

    check if flexslider func exists to solve console error message

--- resource/js/layout-main.js
+++ resource/js/layout-main.js
@@ -15,42 +15,45 @@ $navItemHasDropdown.click(function() {
 	$(this).find('.nav-dropdown-menu').toggle();
 });
 
-/* ===================================================
- * 首頁 Header - flexslider initialization
- * ===================================================
- */
+// Initialize if FlexSlider is Loaded.
+if($.isFunction($.fn.flexslider)) {
+	/* ===================================================
+	 * 首頁 Header - flexslider initialization
+	 * ===================================================
+	 */
 
-$('#index-header > .flexslider').flexslider({
-	animation: 'fade',
-	directionNav: false,
-	slideshowSpeed: 8000,
-	animationSpeed: 1000,
-});
+	$('#index-header > .flexslider').flexslider({
+		animation: 'fade',
+		directionNav: false,
+		slideshowSpeed: 8000,
+		animationSpeed: 1000,
+	});
 
-/* ===================================================
- * 試用品申請｜Trial - flexslider initialization
- * ===================================================
- */
+	/* ===================================================
+	 * 試用品申請｜Trial - flexslider initialization
+	 * ===================================================
+	 */
 
-$('#trial-recommend > .flexslider').flexslider({
-	animation: "slide",
-	directionNav: false,
-	controlNav: false,
-	slideshowSpeed: 4000,
-	animationSpeed: 1000,
-});
+	$('#trial-recommend > .flexslider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: false,
+		slideshowSpeed: 4000,
+		animationSpeed: 1000,
+	});
 
-/* ===================================================
- * 單項商品頁｜Single Product - flexslider initialization
- * ===================================================
- */
+	/* ===================================================
+	 * 單項商品頁｜Single Product - flexslider initialization
+	 * ===================================================
+	 */
 
-$('#single-product-carousel > .flexslider').flexslider({
-	animation: "slide",
-	controlNav: "thumbnails",
-	slideshowSpeed: 4000,
-	animationSpeed: 600,
-});
+	$('#single-product-carousel > .flexslider').flexslider({
+		animation: "slide",
+		controlNav: "thumbnails",
+		slideshowSpeed: 4000,
+		animationSpeed: 600,
+	});
+}
 
 // Initialize accordion if JQuery UI is Loaded.
 if(window.jQuery.ui) {
