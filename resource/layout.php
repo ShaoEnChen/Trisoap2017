2abad11015dbbe7acaca380462d8a0c17dfd2206
ShaoEnChen <seanchen47@gmail.com>
2017-11-30 17:08:19 +0800

    read json to get jumbotron content

---
+++ resource/layout.php
@@ -0,0 +1,148 @@
+<?php
+
+function check_dependency_head($route) {
+	$require_flexslider = ['index', 'trial', 'single_product'];
+	$require_jquery_ui = ['faq', 'partner', 'single_product'];
+
+	if(in_array($route, $require_flexslider)){
+		echo '		<link href="resource/flexslider/flexslider.min.css" rel="stylesheet">';
+	}
+
+	if(in_array($route, $require_jquery_ui)){
+		echo '		<link href="resource/js/jquery-ui-accordion/jquery-ui.min.css" rel="stylesheet">';
+	}
+}
+
+function check_dependency_body($route) {
+	$require_flexslider = ['index', 'trial', 'single_product'];
+	$require_jquery_ui = ['faq', 'partner', 'single_product'];
+	$require_map_api = ['contact'];
+
+	if(in_array($route, $require_flexslider)){
+		echo '		<script src="resource/flexslider/jquery.flexslider-min.js" defer></script>';
+	}
+
+	if(in_array($route, $require_jquery_ui)){
+		echo '		<script src="resource/js/jquery-ui-accordion/jquery-ui.min.js" defer></script>';
+	}
+
+	if(in_array($route, $require_map_api)){
+		echo '		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqLzZouUqN1dWEVR9_75YO6bXL5OuhcRs"></script>';
+		echo '		<script src="resource/js/contact-map.js" defer></script>';
+	}
+}
+
+function get_file_content($directory) {
+	$handle = fopen($directory, 'r');
+	$content = fread($handle, filesize($directory));
+	fclose($handle);
+	return $content;
+}
+
+function include_view_head($route) {
+	require('view/head.html');
+	check_dependency_head($route);
+	require('view/head_finish.html');
+}
+
+function include_view_nav($authority) {
+	// Get Nav content, find '{login_status}' and replace with proper function links
+	$nav_dir = 'view/nav.html';
+	$nav_content = get_file_content($nav_dir);
+
+	// Admin
+	if ($authority == 'A') {
+		$nav_auth_dir = 'view/component/nav/login_admin.html';
+	}
+	// Member
+	elseif ($authority == 'B') {
+		$nav_auth_dir = 'view/component/nav/login_member.html';
+	}
+	// Anonymous
+	else {
+		$nav_auth_dir = 'view/component/nav/login_signup.html';
+	}
+
+	$nav_status_content = get_file_content($nav_auth_dir);
+	$nav_content = str_replace('{login_status}', $nav_status_content, $nav_content);
+	echo $nav_content;
+}
+
+function include_view_jumbotron($route) {
+	if($route === 'index') {
+		include_once('view/component/jumbotron/index_flexslider.html');
+	}
+	else {
+		include_view_image_jumbotron($route);
+	}
+	include_once('view/component/jumbotron/header_finish.html');
+}
+
+function include_view_image_jumbotron($route) {
+	// Get img jumbotron with background image
+	$header_start_dir = 'view/component/jumbotron/img_jumbotron.html';
+	$header_start_content = get_file_content($header_start_dir);
+	$header_start_content = str_replace('{route}', $route, $header_start_content);
+	echo $header_start_content;
+
+	list($title, $subtitle, $btn) = get_img_jumbotron_content($route);
+
+	if(!is_null($title)) {
+		// Get jumbotron title
+		$title_dir = 'view/component/jumbotron/title.html';
+		$title_content = get_file_content($title_dir);
+		if(is_null($subtitle) && is_null($btn)) {
+			// change class name
+			$title_content = str_replace('jumbotron-title', 'jumbotron-only-title', $title_content);
+		}
+		$title_content = str_replace('{route_title}', $title, $title_content);
+		echo $title_content;
+	}
+	if(!is_null($subtitle)) {
+		// Get jumbotron subtitle if $route has subtitle
+		$subtitle_dir = 'view/component/jumbotron/subtitle.html';
+		$subtitle_content = get_file_content($subtitle_dir);
+		$subtitle_content = str_replace('{route_subtitle}', $subtitle, $subtitle_content);
+		echo $subtitle_content;
+	}
+	if(!is_null($btn)) {
+		// Get jumbotron btn text & link if $route has btn
+		$btn_dir = 'view/component/jumbotron/btn.html';
+		$btn_content = get_file_content($btn_dir);
+		$btn_content = str_replace('{route_btn_link}', $btn["link"], $btn_content);
+		$btn_content = str_replace('{route_btn_text}', $btn["text"], $btn_content);
+		echo $btn_content;
+	}
+}
+
+function get_img_jumbotron_content($route) {
+	$content_json_dir = 'resource/json/jumbotron/' . $route . '.json';
+	$content = file_get_contents($content_json_dir);
+	$content_json = json_decode($content, true);
+	$title = $content_json["title"];
+	$subtitle = $content_json["subtitle"];
+	$btn = $content_json["btn"];	// contain btn-link & btn-text if not null
+	return array($title, $subtitle, $btn);
+}
+
+function include_view_content($route) {
+	include_once('view/content/' . $route . '.html');
+}
+
+function include_view_footer($route) {
+	// Get Footer content, find '{company_}'s and replace with proper company info
+	$footer_dir = 'view/footer.html';
+	$footer_content = get_file_content($footer_dir);
+
+	$company_phone = curl_post(array('module' => 'cue', 'target' => 'company_phone'), 'cue');
+	$company_email = curl_post(array('module' => 'cue', 'target' => 'company_email'), 'cue');
+	$company_address = curl_post(array('module' => 'cue', 'target' => 'company_address'), 'cue');
+
+	$footer_content = str_replace('{company_phone}', $company_phone, $footer_content);
+	$footer_content = str_replace('{company_email}', $company_email, $footer_content);
+	$footer_content = str_replace('{company_address}', $company_address, $footer_content);
+	echo $footer_content;
+
+	check_dependency_body($route);
+	require('view/footer_finish.html');
+}
