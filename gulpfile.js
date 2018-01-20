73c4d87bd4783a2c2a7428ee0f57a95918d2a2e2
ShaoEnChen <seanchen47@gmail.com>
2018-01-20 11:33:16 +0800

    use gulp for babel & uglify instead of webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
-gulp.task('default', () => {
+gulp.task('default', ['js']);
 
+gulp.task('js', () => {
+	gulp.src('resource/js/**/*.js')
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(gulp.dest('resource/dist/js/'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const path = require('path');
 
-module.exports = {
-	entry: './resource/js/trisoap.js',
-	output: {
-		path: path.resolve(__dirname, 'resource/dist'),
-		filename: 'js/[name].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['babel-preset-env']
-					}
-				}
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader', 'postcss-loader', 'sass-loader', 'resolve-url-loader']
-				})
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: '[path][name].[ext]'
-					}
-				}
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('css/[name].css')
-	]
-};
+// module.exports = {
+// 	entry: {
+// 		'main': './resource/js/view/general.js',
+// 		'function': './resource/js/view/function.js'
+// 	},
+// 	output: {
+// 		path: path.resolve(__dirname, 'resource/dist/js/'),
+// 		filename: '[name].js'
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.js$/,
+// 				exclude: /(node_modules|bower_components)/,
+// 				use: {
+// 					loader: 'babel-loader'
+// 				}
+// 			},
+// 			{
+// 				test: /\.(scss|css)$/,
+// 				use: ExtractTextPlugin.extract({
+// 					fallback: 'style-loader',
+// 					use: ['css-loader', 'postcss-loader', 'sass-loader', 'resolve-url-loader']
+// 				})
+// 			},
+// 			{
+// 				test: /\.(png|jpg|gif)$/,
+// 				use: {
+// 					loader: 'file-loader',
+// 					options: {
+// 						name: '[path][name].[ext]'
+// 					}
+// 				}
+// 			}
+// 		]
+// 	},
+// 	plugins: [
+// 		new ExtractTextPlugin('[name].css')
+// 	]
+// };
