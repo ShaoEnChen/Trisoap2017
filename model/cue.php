770de0d471f32098c4b70673f6e4cd10746753b3
Tim <a0922825881@gmail.com>
2018-04-17 15:23:52 +0800

    necessary data update

--- model/cue.php
+++ model/cue.php
@@ -61,11 +61,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 			$account = $_GET['account'];
 			$sql = mysql_query("SELECT * FROM CUSMAS WHERE EMAIL='$account'");
 			$fetch = mysql_fetch_array($sql);
-			if (in_array($fetch['CUSTYPE'], array('A', 'B', 'C', 'D')) && in_array($fetch['KNOWTYPE'], array('A', 'B', 'C', 'D', 'E')) && in_array($fetch['CUSSEX'], array('M', 'F'))) {
-				echo 'valid';
+			if (in_array($fetch['CUSTYPE'], array('A', 'B', 'C', 'D')) || in_array($fetch['KNOWTYPE'], array('A', 'B', 'C', 'D', 'E')) || in_array($fetch['CUSSEX'], array('M', 'F'))) {
+				echo 'invalid';
 			}
 			else {
-				echo 'invalid';
+				echo 'valid';
 			}
 			return;
 		}
@@ -221,11 +221,11 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$account = $_POST['account'];
 			$sql = mysql_query("SELECT * FROM CUSMAS WHERE EMAIL='$account'");
 			$fetch = mysql_fetch_array($sql);
-			if (in_array($fetch['CUSTYPE'], array('A', 'B', 'C', 'D')) && in_array($fetch['KNOWTYPE'], array('A', 'B', 'C', 'D', 'E')) && in_array($fetch['CUSSEX'], array('M', 'F'))) {
-				echo 'valid';
+			if (in_array($fetch['CUSTYPE'], array('A', 'B', 'C', 'D')) || in_array($fetch['KNOWTYPE'], array('A', 'B', 'C', 'D', 'E')) || in_array($fetch['CUSSEX'], array('M', 'F'))) {
+				echo 'invalid';
 			}
 			else {
-				echo 'invalid';
+				echo 'valid';
 			}
 			return;
 		}
