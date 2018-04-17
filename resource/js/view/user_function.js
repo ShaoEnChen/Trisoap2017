b1437cfe3a0f7fcf3604fc8e5d86b57750557ba0
Tim <a0922825881@gmail.com>
2018-04-17 16:42:30 +0800

    price calculation update

--- resource/js/view/user_function.js
+++ resource/js/view/user_function.js
@@ -170,12 +170,6 @@ function memberSignup() {
 	inputs.required.password1 = document.getElementById("password1");
 	inputs.required.password2 = document.getElementById("password2");
 	inputs.optional.phone = document.getElementById("phone");
-	inputs.optional.skintype = document.getElementById("skintype");
-	inputs.optional.birth = document.getElementById("birth");
-	inputs.optional.add = document.getElementById("address");
-	inputs.optional.taxid = document.getElementById("taxid");
-	inputs.optional.knowtype = document.getElementById("knowtype");
-	inputs.optional.notice = document.getElementById("notice");
 
 	for(let fieldType in inputs) {
 		for(let field in inputs[fieldType]) {
