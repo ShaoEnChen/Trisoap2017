715245121a3fec96d37a97a3efa0b543034abdc2
ShaoEnChen <seanchen47@gmail.com>
2018-04-25 11:52:42 +0800

    fix fb-login btn settings

--- resource/js/view/user_function/member_signin.js
+++ resource/js/view/user_function/member_signin.js
@@ -29,10 +29,10 @@ window.fbAsyncInit = function() {
 };
 
 // Load the SDK asynchronously
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/zh_TW/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.12&appId=403462720092445';
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
